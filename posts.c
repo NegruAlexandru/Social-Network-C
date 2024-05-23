805ac91f4ee8afd79c3e4d6e1371bdf635f848ee
rici029 <bogdy294@gmail.com>
2024-05-23 20:04:34 +0300

    posts.c gata

--- posts.c
+++ posts.c
@@ -6,50 +6,37 @@
 #include "posts_functions.h"
 #include "users.h"
 
-// free the memory allocated for the posts and the struct used to store them
 void free_node(g_node_t *node)
 {
 	if (!node)
 		return;
 
-	// Free children nodes recursively
 	for (int i = 0; i < node->n_children; i++)
 		free_node(node->children[i]);
 
-	// Free the node's children array
 	free(node->children);
 
-	// Free the node itself
 	free(node);
 }
 
-// Function to free the entire posts array
 void free_posts(post_array_t *posts)
 {
-    for (unsigned int i = 0; i < posts->size; i++) {
-        if (posts->array[i]) {
-            // Free the post's title
+	for (unsigned int i = 0; i < posts->size; i++) {
+		if (posts->array[i]) {
 			if (posts->array[i]->title)
-            	free(posts->array[i]->title);
+				free(posts->array[i]->title);
 
-			// Free the event tree if it exists
 			if (posts->array[i]->events) {
-				// Free the root node (which recursively frees the entire tree)
 				free_node(posts->array[i]->events->root);
-
-				// Free the events structure
 				free(posts->array[i]->events);
 			}
 
-			// Free the post structure
 			free(posts->array[i]);
 		}
 	}
 
-	// Free the posts array itself
 	free(posts->array);
 
-	// Free the posts structure
 	free(posts);
 }
 
